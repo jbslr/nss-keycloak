f2f2abd7c803f38fbd4d11b47a18ca2c7df959e2
Jens Bassler <jens.bassler@protonmail.com>
2024-02-24 19:47:12 +0100

    Update config_integration and test config file
    * update config file: use correct data for the dockerized
    dev-test environment.
    * adjust config_integration test accordingly

--- nss-keycloak/tests/config_integration.rs
+++ nss-keycloak/tests/config_integration.rs
@@ -7,12 +7,12 @@ fn test_get_config_path_from_env() {
 
     temp_env::with_var("NSSKEYCLOAK_CONFIG_FILE", Some("tests/files/config.toml"), || {
         // config.keycloak
-        assert_eq!(CONFIG.keycloak.realm, "myrealm");
-        assert_eq!(CONFIG.keycloak.client_id, "myclient");
-        assert_eq!(CONFIG.keycloak.client_secret, "mysecret");
-        assert_eq!(CONFIG.keycloak.url, "http://localhost:8080/auth");
-        assert_eq!(CONFIG.keycloak.username, "myuser");
-        assert_eq!(CONFIG.keycloak.password, "mypassword");
+        assert_eq!(CONFIG.keycloak.realm, "test");
+        assert_eq!(CONFIG.keycloak.client_id, "nss-client");
+        assert_eq!(CONFIG.keycloak.client_secret, "UO2i2h5Vku3oQBvFIlsMG23pIMZyRJOi");
+        assert_eq!(CONFIG.keycloak.url, "http://localhost:8080");
+        assert_eq!(CONFIG.keycloak.username, "nss-user");
+        assert_eq!(CONFIG.keycloak.password, "nss-user");
         // config.mapping
         assert_eq!(CONFIG.mapping.user_home, "homedirectory");
         assert_eq!(CONFIG.mapping.user_shell, "defaultshell");
