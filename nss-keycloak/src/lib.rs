b3c4960e76da4e00c1c900b0e21713503d7a5614
Jens Bassler <jens.bassler@protonmail.com>
2024-02-24 16:52:44 +0100

    Create static global CONFIG in lib.rs
    This serves as a singleton for the configuration, and is
    lazily initialized on first use. Prevents the need to
    reload the configuration from disk on every call to
    a library function.

--- nss-keycloak/src/lib.rs
+++ nss-keycloak/src/lib.rs
@@ -1,1 +1,9 @@
 mod config;
+
+use lazy_static::lazy_static;
+
+lazy_static! {
+    // TODO: Remove pub visibility once the plugin is implemented
+    pub static ref CONFIG: config::Config = config::load_config()
+        .expect("Failed to load plugin configuration");
+}
