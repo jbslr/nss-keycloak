fa3dd0f4b9f23f668e3034b8e42c5fe835a9b213
Jens Bassler <jens.bassler@protonmail.com>
2024-02-25 21:16:51 +0100

    Add keycloak authentication for the plugin
    This commit adds the keycloak authentication for the plugin. It adds the
    keycloak.auth module. This module contains the KeycloakAuth struct.
    This struct is responsible for authenticating the user using the
    Keycloak server. An access token can be obtained from the KeycloakAuth
    instance by calling the get_access_token method. This method will
    return a valid access token by refreshing or re-authenticating
    the token if necessary.
    
    Adds a static lazily-initialized KeycloakAuth instance to the
    nss_keycloak module (lib.rs).
    
    Adds a integration test for the KeycloakAuth singleton in lib.rs.

--- nss-keycloak/src/lib.rs
+++ nss-keycloak/src/lib.rs
@@ -1,4 +1,7 @@
-mod config;
+pub mod config;
+pub mod keycloak;
+
+use std::sync::Mutex;
 
 use lazy_static::lazy_static;
 
@@ -6,4 +9,10 @@ lazy_static! {
     // TODO: Remove pub visibility once the plugin is implemented
     pub static ref CONFIG: config::Config = config::load_config()
         .expect("Failed to load plugin configuration");
+
+    pub static ref AUTH: Mutex<keycloak::auth::KeycloakAuth<'static>> = 
+        Mutex::new(
+            keycloak::auth::KeycloakAuth::new(&CONFIG.keycloak)
+                .expect("Failed to initialize Keycloak authentication")
+            );
 }
---
+++ nss-keycloak/tests/keycloak_auth_integration.rs
@@ -0,0 +1,78 @@
+use std::ops::{Add, DerefMut};
+use std::time::Duration;
+
+use anyhow::Result;
+
+use mock_instant::MockClock;
+use nss_keycloak::{AUTH, CONFIG};
+use nss_keycloak::keycloak::auth::{KeycloakAuth, TokenProvider};
+use nss_keycloak::config::KeycloakConfig;
+
+/// validate token using the Keycloak token introspection endpoint
+fn validate_token(
+    config: &KeycloakConfig, 
+    access_token: &str,
+) -> Result<()> {
+
+    #[derive(serde::Deserialize)]
+    struct KeycloakIntrospectionResponse {
+        active: bool,
+    }
+
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .post(
+            &format!("{}/realms/{}/protocol/openid-connect/token/introspect", 
+            config.url, 
+            config.realm,
+        ))
+        .form(&[
+            ("token", access_token),
+            ("client_id", &config.client_id),
+            ("client_secret", &config.client_secret),
+        ])
+        .send()?;
+    let introspection = serde_json::from_str::<KeycloakIntrospectionResponse>(&response.text()?)?;
+    if introspection.active {
+        Ok(())
+    } else {
+        Err(anyhow::anyhow!("Token introspection failed"))
+    }
+}
+
+#[test]
+fn test_keycloak_auth_get_token() {
+    // warn if the 'mock' feature is not enabled
+    // required for modifying the system time
+    #[cfg(not(feature = "mock"))]
+    panic!("This test requires the 'mock' feature to be enabled");
+
+    #[cfg(feature = "mock")]
+    temp_env::with_var("NSSKEYCLOAK_CONFIG_FILE", Some("tests/files/config.toml"), || {
+        let access_token01 = AUTH.lock().unwrap().get_access_token().unwrap().to_owned();
+        validate_token(&CONFIG.keycloak, &access_token01)
+            .expect("Token validation failed");
+
+        // expiration time of the access token (plus 1 second to be above that)
+        let access_token_exires_in = AUTH.lock().unwrap().access_token_expires_in().unwrap().add(Duration::from_secs(1));
+        MockClock::advance_system_time(access_token_exires_in);
+        let access_token02 = AUTH.lock().unwrap().get_access_token().unwrap().to_owned();
+        validate_token(&CONFIG.keycloak, &access_token02)
+            .expect("Token validation failed");
+
+        // the access tokens should be different because it has been updated 
+        // using the refresh operation
+        assert_ne!(access_token01, access_token02);
+
+        // expiration time of the refresh token (plus 1 second to be above that)
+        let refresh_token_expires_in = AUTH.lock().unwrap().refresh_token_expires_in().unwrap().add(Duration::from_secs(1));
+        MockClock::advance_system_time(refresh_token_expires_in);
+        let access_token03 = AUTH.lock().unwrap().get_access_token().unwrap().to_owned();
+        validate_token(&CONFIG.keycloak, &access_token03)
+            .expect("Token validation failed");
+
+        // the access tokens should be different because it has been updated 
+        // with a full re-authentication
+        assert_ne!(access_token02, access_token03);
+    });
+}
