e8ce3ac6d29565d40ff1eebf45c154ab2da74eb8
Jens Bassler <jens.bassler@protonmail.com>
2024-03-03 21:59:52 +0100

    Implement the libnss:groups::GroupHooks trait
    Fetches group data from Keycloak, returns it as libnss Group objects

--- nss-keycloak/src/group.rs
+++ nss-keycloak/src/group.rs
@@ -1,11 +1,42 @@
 use libnss::interop::Response;
 use libnss::group::{Group, GroupHooks};
 
-struct KeycloakNssGroup;
+use crate::keycloak::auth::TokenProvider;
+use crate::keycloak::groups::{list_groups, KeycloakGroup};
+
+pub struct KeycloakNssGroup;
+
+impl From<KeycloakGroup> for Group {
+    fn from(group: KeycloakGroup) -> Self {
+        Group {
+            name: group.name,
+            passwd: "x".to_string(),
+            gid: group.gid,
+            members: group.members,
+        }
+    }
+}
 
 impl GroupHooks for KeycloakNssGroup {
+
+    /// Get all groups from Keycloak
+    /// calls keycloak::list_groups underneath
     fn get_all_entries() -> Response<Vec<Group>> {
-        todo!()
+        match list_groups(
+            &crate::CONFIG.keycloak,
+            &crate::CONFIG.mapping,
+            crate::AUTH.lock().unwrap().get_access_token().unwrap(),
+        ) {
+            Ok(groups) => Response::Success(
+                groups.into_iter()
+                .map(|group| Group::from(group))
+                .collect()
+            ),
+            Err(err) => {
+                log::error!("Failed to get all groups: {:?}", err);
+                Response::Unavail
+            },
+        }
     }
 
     fn get_entry_by_gid(gid: libc::gid_t) -> Response<Group> {
